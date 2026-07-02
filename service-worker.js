0c889e1d1262b992e6437acca1f8b9bebbaac3de
olegsalamander-dotcom <oleg.salamander@gmail.com>
2026-07-02 22:57:25 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-// Изменили версию на v1.6. Теперь телефон поймет, что файлы обновились!
-const CACHE_NAME = 'fotoprogulka-02.07.2026-v3.2'; 
+const CACHE_NAME = 'fotoprogulka-02.07.2026-v4'; 
 const ASSETS = [
   './',
   './index.html',
